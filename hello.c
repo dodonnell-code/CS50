5ddb6ab43b3cb557faee11e9b87ca29c21229d6a
dodonnell-code <55148092+dodonnell-code@users.noreply.github.com>
2020-01-15 13:19:41 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
+#include <cs50.h> //required for string functions in this example.
 #include <stdio.h>
 
 int main(void)
 {
     printf("Hello, World\n");
+    
+    string answer = get_string("What is your name?\n");
+    printf("Hello, %s\n", answer);
     
 }
